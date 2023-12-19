e322e7329b608d91634862962123e98ecc424784
Arpeeee <brian9609@gmail.com>
2023-12-19 19:57:50 +0800

    spec test

---
+++ src/views/SearchBar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="d-flex justify-content-center align-items-center vh-100">
+    <input type="text" class="form-control" v-model="search" placeholder="Search..." />
+  </div>
+</template>
+
+<style scoped="scss"></style>
